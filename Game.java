7a189335e7e2961dc14da0a54d3dc8c2195040e0
duy <duy.hoangduc97@gmail.com>
2019-08-03 15:01:07 +0700

    added index.html

---
+++ Game.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+
+public class Game
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        String y ="hello";
+        if(x.equals(y));
+        {
+            System.out.println("bye");
+        }
+        
+    }
+}
